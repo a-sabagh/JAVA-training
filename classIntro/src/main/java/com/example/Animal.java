f77f3b8c83ba709126381168279d22c554b2b3af
asabagh <a.sabagh72@gmail.com>
2018-05-03 14:44:42 +0430

    introduce java class

---
+++ classIntro/src/main/java/com/example/Animal.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class Animal {
+
+    public static void main(String args[]){
+        lion first = new lion();
+
+        int output = first.sum(10,15);
+
+        System.out.println("output is: " + output);
+
+    }
+}
---
+++ classIntro/src/main/java/com/example/lion.java
@@ -0,0 +1,11 @@
+package com.example;
+
+/**
+ * Created by asabagh on 3/22/18.
+ */
+
+public class lion {
+    public int sum(int a , int b){
+        return (a+b);
+    }
+}
