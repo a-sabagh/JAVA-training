5059ad7ec1078a9cb049d025c4a9fa11715c8728
asabagh <a.sabagh72@gmail.com>
2018-05-03 14:49:54 +0430

    encapulation in java class

---
+++ encapsulation/src/main/java/com/example/A.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class A {
+    private int x = 10;
+
+    public int getX(){
+        return this.x;
+    }
+
+    public void setX(int x){
+        this.x = x;
+    }
+}
---
+++ encapsulation/src/main/java/com/example/B.java
@@ -0,0 +1,14 @@
+package com.example;
+
+/**
+ * Created by asabagh on 4/4/18.
+ */
+
+public class B {
+    public static void main(String args[]){
+        A a = new A();
+        a.setX(15);
+        System.out.println(a.getX());
+
+    }
+}
