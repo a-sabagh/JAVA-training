d290f4885cd32113f814eca9a385eee6524fd639
asabagh <a.sabagh72@gmail.com>
2018-05-03 14:50:42 +0430

    final keyword for restrict user to using class and variable and methods

---
+++ finall-keyword/src/main/java/com/example/A.java
@@ -0,0 +1,17 @@
+package com.example;
+
+/**
+ * Created by asabagh on 4/26/18.
+ */
+
+public final class A {
+    public final int a;
+
+    public A(int a) {
+        this.a = a;
+    }
+
+    public final void print_a(){
+        System.out.println("a is:" + a);
+    }
+}
