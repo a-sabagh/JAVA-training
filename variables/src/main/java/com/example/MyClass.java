c53fa73a0f8e083ac3c11be768e551d496b68f0f
asabagh <a.sabagh72@gmail.com>
2018-05-03 15:02:55 +0430

    intro variable in java

---
+++ variables/src/main/java/com/example/MyClass.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class MyClass {
+    public static void main(String args[]){
+        float first_num,second_num,answer;
+        first_num = 10.5f;
+        second_num = 20.8f;
+        answer = first_num/second_num;
+        System.out.println("Total: " + answer);
+    }
+
+}
