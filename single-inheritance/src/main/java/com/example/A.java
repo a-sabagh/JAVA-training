c82705610fcbc8389f66f468563bbee2ff0f471f
asabagh <a.sabagh72@gmail.com>
2018-05-03 14:59:11 +0430

    single inheritance - simple

---
+++ single-inheritance/src/main/java/com/example/A.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class A {
+
+    int data = 10;
+
+    public static void main(String args[]){
+        //defind bClass variable
+        B bClass = new B();
+        //call displayData method
+        bClass.displayData();
+    }
+
+}
---
+++ single-inheritance/src/main/java/com/example/B.java
@@ -0,0 +1,12 @@
+package com.example;
+
+/**
+ * Created by asabagh on 3/22/18.
+ */
+
+public class B extends A{
+
+    public void displayData(){
+         System.out.println("data is: " + data);
+    }
+}
