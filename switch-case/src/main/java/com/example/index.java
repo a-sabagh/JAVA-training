890ba8fc3b3dd697b266aadf0d337953bdc941bc
asabagh <a.sabagh72@gmail.com>
2018-05-03 15:00:46 +0430

    switch case conditional statements

---
+++ switch-case/src/main/java/com/example/index.java
@@ -0,0 +1,19 @@
+package com.example;
+
+public class index {
+    public static void main(String args[]){
+        int a = 3;
+        switch (a){
+            case 1:
+                System.out.println("one");
+                break;
+            case 2:
+                System.out.println("tow");
+                break;
+            case 3:
+                System.out.println("three");
+                break;
+            default:
+        }
+    }
+}
