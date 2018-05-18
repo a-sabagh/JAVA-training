b2e6a7f45e6c068adaf8bd37dae1946aa896bf04
asabagh <a.sabagh72@gmail.com>
2018-05-18 16:26:07 +0430

    foreach loop known by for keyword

--- foreach-loop/src/main/java/com/example/MyClass.java
+++ foreach-loop/src/main/java/com/example/MyClass.java
@@ -1,4 +1,10 @@
 package com.example;
 
 public class MyClass {
+    public static void main(String args[]){
+        String[] N = {"atiyeh","mahla","sahar"};
+        for(String name: N){
+            System.out.println(name);
+        }
+    }
 }
