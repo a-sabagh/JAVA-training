6ed618e6c1212aaf0c8236c6f5562ac806f93f11
asabagh <a.sabagh72@gmail.com>
2018-05-18 16:19:01 +0430

    adding nested loop

--- nested-loop/src/main/java/com/example/MyClass.java
+++ nested-loop/src/main/java/com/example/MyClass.java
@@ -1,4 +1,12 @@
 package com.example;
 
 public class MyClass {
+    public static void main(String arg[]){
+        for(int i=1;i<10;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(" * ");
+            }
+            System.out.print("\n");
+        }
+    }
 }
