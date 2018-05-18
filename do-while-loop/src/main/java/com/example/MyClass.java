aa372e38c2a9d50ab00190ae59585e8d262fd298
asabagh <a.sabagh72@gmail.com>
2018-05-18 16:18:42 +0430

    adding do...while loop

---
+++ do-while-loop/src/main/java/com/example/MyClass.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class MyClass {
+    public static void main(String args[]){
+        int i = 9;
+
+        do{
+            System.out.println(i);
+            i--;
+        }while(i > -1);
+    }
+}
